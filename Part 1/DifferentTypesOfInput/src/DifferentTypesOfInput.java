73348af191aac3701e451594c978fc8bda467f95
JustinLinuxLaptop <herrjustin0@gmail.com>
2026-07-24 16:52:41 -0500

    Add "different types of integers" exercise

---
+++ Part 1/DifferentTypesOfInput/src/DifferentTypesOfInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DifferentTypesOfInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a string:");
+        String text = scanner.nextLine();
+        System.out.println("Give an integer:");
+        int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give a double:");
+        double deciNum = Double.valueOf(scanner.nextLine());
+        System.out.println("Give a boolean:");
+        boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
+
+        System.out.println("You gave the string " + text);
+        System.out.println("You gave the string " + number);
+        System.out.println("You gave the string " + deciNum);
+        System.out.println("You gave the string " + trueOrFalse);
+    }
+}
