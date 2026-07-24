96f44300505d5fbd2f31e17728c0246336b3704e
Justin Herr <herrjustin0@gmail.com>
2026-07-24 13:04:48 -0500

    Initial commit

---
+++ Part 1/Sandbox/src/SandboxTest.java
@@ -0,0 +1,7 @@
+public class SandboxTest {
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Hello, Java!");
+        //System.out.println();
+    }
+}
